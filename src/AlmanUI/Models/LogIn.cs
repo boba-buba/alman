d7916dfa99110e52355952dc193abe296be27b1b
Nataliia Korop <n.corop08@gmail.com>
2024-07-21 09:26:44 +0200

    delete old ui project

--- src/AlmanUI/Models/LogIn.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AlmanUi.Models;
-
-public class LogIn
-{
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-}
--- src/AlmanUI/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AlmanUI.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    //public string Greeting => "Привет из Алмана";
-}
