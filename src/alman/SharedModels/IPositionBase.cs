4345cefb525d07326eb226dd3106c1534bdf8d0b
Nataliia Korop <n.corop08@gmail.com>
2024-08-03 21:11:01 +0200

    UI: YMActivities calendar works, Converter still no, Position, Staff beginning

---
+++ src/alman/SharedModels/IPositionBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Alman.SharedModels;
+
+public interface IPositionBase
+{
+    public int PositionId { get; set; }
+
+    public string? PositionName { get; set; }
+
+    public int? PositionSalary { get; set; }
+}
---
+++ src/alman/SharedModels/IStaffMemberBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Alman.SharedModels;
+
+public interface IStaffMemberBase
+{
+    public int StaffMemberId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public int? PositionId { get; set; }
+
+    public int State { get; set; }
+}
