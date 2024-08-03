20a97e1c1460c8ed3f68640d17f6e88a90e72366
Nataliia Korop <n.corop08@gmail.com>
2024-08-03 11:32:28 +0200

    UI: YMActivities in process: save ok, converter doesnt work (wasnt pushed first time)

--- src/AlmanUI/Controls/YearMonthActivitiesControl.cs
+++ src/AlmanUI/Controls/YearMonthActivitiesControl.cs
@@ -3,6 +3,10 @@ using Alman.SharedDefinitions;
 using Business;
 using System.Collections.Generic;
 using AlmanUI.Models;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Diagnostics;
 namespace Avalonia.Controls;
 
 
@@ -14,9 +18,71 @@ public static class YearMonthActivitiesControl
         return activiries;
     }
 
+    private static ReturnCode AddYearMonthActivities(IReadOnlyList<IYearMonthActivityBase> ymActivities)
+    {
+        return BusinessYearMonthActivitiesApi.AddYearMonthActivities(ymActivities);
+    }
+
     public static ReturnCode UpdateYearMonthActivities(IReadOnlyList<YearMonthActivityUI> activities)
     {
         return BusinessYearMonthActivitiesApi.UpdateYMActivities(activities);
         
+    }
+
+    public static ReturnCode SaveYearMonthActivities(IReadOnlyList<IYearMonthActivityBase> ymActivitiesToSave, int year, int month)
+    {
+        ReturnCode retval = ReturnCode.ERR;
+        var ymActivitiesFromDb = BusinessYearMonthActivitiesApi.GetYMActivities(year, month);
+        int dbCount = ymActivitiesFromDb.Count;
+        int difference = ymActivitiesToSave.Count - dbCount;
+
+        if (dbCount > 0)
+        {
+            var updatedYMActivities = ymActivitiesToSave.Where(ymAct => ymAct.InGroup(ymActivitiesFromDb)).ToList();
+
+            retval = BusinessYearMonthActivitiesApi.UpdateYMActivities(updatedYMActivities);
+            if (retval != ReturnCode.OK)
+            {
+                Debug.WriteLine($"Smth went wrong with updating {nameof(IYearMonthActivityBase)}");
+                return retval;
+            }
+        }
+
+        if (difference > 0)
+        {
+
+            var newYMActivities = ymActivitiesToSave.Where(ymAct => !ymAct.InGroup(ymActivitiesFromDb)).ToList();
+            retval = AddYearMonthActivities(newYMActivities);
+        }
+
+        return retval;
+    }
+
+}
+
+
+public static class YearMonthActivitiesExtensions
+{
+    public static bool DbEquals(this IYearMonthActivityBase item,  IYearMonthActivityBase other)
+    {
+        if (item.YmchildId !=  other.YmchildId) 
+            return false;
+        if (item.YmactivityId != other.YmactivityId) 
+            return false;
+        if (item.Month !=  other.Month) 
+            return false;
+        if (item.Year != other.Year) 
+            return false;
+        return true;
+    }
+
+    public static bool InGroup(this IYearMonthActivityBase item, IReadOnlyList<IYearMonthActivityBase> group)
+    {
+        foreach (var groupItem in group)
+        {
+            if (item.DbEquals(groupItem)) 
+                return true;
+        }
+        return false;
     }
 }
