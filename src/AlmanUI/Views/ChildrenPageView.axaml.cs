40dacb1291e3879e4e850f8893814c59779c3cc8
Nataliia Korop <n.corop08@gmail.com>
2024-08-05 21:42:14 +0200

    UI: Children OK, ContractType combobox still not

--- src/AlmanUI/Views/ChildrenPageView.axaml.cs
+++ src/AlmanUI/Views/ChildrenPageView.axaml.cs
@@ -1,6 +1,9 @@
+using AlmanUI.ViewModels;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using System.Linq;
 
 namespace AlmanUI.Views;
 
@@ -9,5 +12,10 @@ public partial class ChildrenPageView : UserControl
     public ChildrenPageView()
     {
         InitializeComponent();
+        this.DataContext = new ChildrenPageViewModel();
+        var col = ChildrenDataGrid.Columns.Last();
+        col.Bind(ComboBox.ItemsSourceProperty, new Binding { Path = "DataContext.ChildrenContractNames", Source = DataContext });
     }
+
+
 }
